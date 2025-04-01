4200fe8600ecfa4aacb9d98b2f17d9a08faa9d28
Gariscat <xl3133@nyu.edu>
2025-04-01 20:38:42 +0800

    randomization-script-completed

---
+++ scripts/reinforcement_learning/skrl/exp_stochastic_cabinet.py
@@ -0,0 +1,23 @@
+from itertools import product
+import subprocess
+
+
+if __name__ == "__main__":
+    seeds = (2022, 2024, 2026)
+    random_rotation_z_ranges = ((-15, 15), (-30, 30), (-60, 60))
+    random_offset_ranges = ((-0.1, 0.1), (-0.2, 0.2), (-0.5, 0.5))
+    
+    for seed, random_rotation_z_range, random_offset_range in product(seeds, random_rotation_z_ranges, random_offset_ranges):
+        command_components = [
+            "python",
+            "train_stochastic_cabinet.py",
+            "--seed", str(seed),
+            "--random_rotation_z_range", ' '.join(map(str, random_rotation_z_range)),
+            "--random_offset_x_range", ' '.join(map(str, random_offset_range)),
+            "--random_offset_y_range", ' '.join(map(str, random_offset_range)),
+            "--random_offset_z_range", ' '.join(map(str, random_offset_range)),
+            "--headless",
+        ]
+        command = ' '.join(command_components)
+        print(f"Running command: {command}")
+        subprocess.call(command, shell=True)
